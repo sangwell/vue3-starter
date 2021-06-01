10b62018c1079054b3ac5855cfa74e90351802f5
Jim Jin <jjn@mm-software.com>
2021-06-01 17:31:04 +0800

    device page

--- src/components/Device.vue
+++ src/components/Device.vue
@@ -1,10 +1,20 @@
 <template>
     <h1>Device page</h1>
+    <ul id="array-rendering">
+        <li v-for="(item,index) in devices">
+            {{index+1}} , {{ item }}
+        </li>
+    </ul>
 </template>
 
 <script>
     export default {
-        name: "Device"
+        name: "Device",
+        data() {
+            return {
+                devices: ['Device A', 'Device B', 'Device C']
+            }
+        }
     }
 </script>
 
