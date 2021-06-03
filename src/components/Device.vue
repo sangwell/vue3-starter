5958d7d7ea2685601393fd8930764642081c041f
Jim Jin <jjn@mm-software.com>
2021-06-03 18:30:33 +0800

    device page

--- src/components/Device.vue
+++ src/components/Device.vue
@@ -1,5 +1,7 @@
 <template>
     <h1>Device page</h1>
+    <input v-model="message" placeholder="edit me"/>
+    <button type="button" @click="add()">Add</button>
     <ul id="array-rendering">
         <li v-for="(item,index) in devices">
             {{index+1}} , {{ item }}
@@ -8,11 +10,41 @@
 </template>
 
 <script>
+    import {reactive, onUnmounted, onUpdated, onMounted, toRefs} from 'vue';
+
     export default {
         name: "Device",
+        setup() {
+            onMounted(() => {
+                console.log('mounted!')
+            });
+            onUpdated(() => {
+                console.log('updated!')
+            });
+            onUnmounted(() => {
+                console.log('unmounted!')
+            });
+        },
         data() {
             return {
+                message: '',
                 devices: ['Device A', 'Device B', 'Device C']
+            }
+        },
+        methods: {
+            add() {
+                console.log('add');
+                // this.devices.push(this.message);
+                // this.message = '';
+                this.$http.get('https://b-gold-cdn.xitu.io/favicons/v2/manifest.json').then(response => {
+
+                    console.log(response);
+                    // get body data
+                    // this.someData = response.body;
+
+                }, response => {
+                    // error callback
+                });
             }
         }
     }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
 import {createApp} from 'vue'
 import App from './App.vue'
 import {router} from './route'
 
+// Vue.use(VueResource);
+
 const app = createApp(App);
 app.use(router);
+// app.use(VueResource);
 app.mount('#app');
